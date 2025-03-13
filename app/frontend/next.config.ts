3ebc778b633331072442c20ee008a212df51da73
Daniel Monroy <fernandodms04@gmail.com>
2025-03-13 00:06:33 -0600

    Allow images from anywhere

--- app/frontend/next.config.ts
+++ app/frontend/next.config.ts
@@ -1,16 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  env: {
-    NEXT_PUBLIC_API_URL:
-      process.env.NEXT_PUBLIC_API_URL || "http://localhost:5001",
-  },
-  // While Next.js automatically uses process.env.PORT,
-  // we can make sure it's consistently documented here
-  server: {
-    port: parseInt(process.env.PORT || "3000", 10),
-    host: "0.0.0.0",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
   },
 };
 
