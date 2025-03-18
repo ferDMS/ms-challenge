c03c3a9ac11d240075f3049f93368d117fed9cf6
Daniel Monroy <fernandodms04@gmail.com>
2025-03-18 03:40:56 -0600

    Initial DB config

---
+++ app/backend/db/config.py
@@ -0,0 +1,32 @@
+# Database configuration settings
+
+# Container definitions
+CONTAINERS = {
+    # 'participants': {
+    #     'name': 'participants',
+    #     'partition_key': '/id'
+    # },
+    # 'coaches': {
+    #     'name': 'coaches',
+    #     'partition_key': '/id'
+    # },
+    'sessions': {
+        'name': 'sessions',
+        'partition_key': '/participantId'
+    },
+    # 'jobs': {
+    #     'name': 'jobs', 
+    #     'partition_key': '/id'
+    # },
+    # 'job_matches': {
+    #     'name': 'job_matches',
+    #     'partition_key': '/participantId'
+    # },
+    # 'documents': {
+    #     'name': 'documents',
+    #     'partition_key': '/id'
+    # }
+}
+
+# Database name
+DB_NAME = 'ms-challenge'
